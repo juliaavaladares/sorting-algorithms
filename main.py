5fac89ec1708f5a2752e43e6ef053965ebd6a2f7
Júlia <julia.valadares99@gmail.com>
2023-05-11 17:05:16 -0300

    add new items

---
+++ main.py
@@ -0,0 +1,16 @@
+from utils import getDados
+
+
+if __name__ == '__main__':
+    print("AAAAAAAAA")
+    # Busca dados da base 
+    #arr = getDados()
+    getDados()
+    # # Exibindo a lista antes da ordenação
+    # print("Lista antes da ordenação: ", arr)
+    
+    # # Ordenando a lista usando bubble sort
+    # bubble_sort(arr)
+    
+    # # Exibindo a lista após a ordenação
+    # print("Lista após a ordenação: ", arr)
